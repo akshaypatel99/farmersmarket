7fadd0370934b9eb5a8bd4bac93295285fe4d285
akshaypatel99 <akshaypatel99@gmail.com>
2020-10-30 11:25:25 +0000

    Bootswatch, Header, Navbar and Footer added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,21 @@
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+
 const App = () => {
-  return (
-    <div className="App">
-      <>
-        <h1>Welcome to Farmer's Market</h1>
-      </>
-    </div>
-  );
-}
+	return (
+		<div className='App'>
+			<>
+				<Header />
+				<main className='py-3'>
+					<Container>
+						<h1>Welcome to Farmer's Market</h1>
+					</Container>
+				</main>
+				<Footer />
+			</>
+		</div>
+	);
+};
 
 export default App;
