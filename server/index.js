d43dd926b598b3018fca03540c92ec5b04b767f6
akshaypatel99 <akshaypatel99@gmail.com>
2020-11-11 13:55:33 +0000

    Preparing for deployment

--- server/index.js
+++ server/index.js
@@ -25,10 +25,6 @@ if (process.env.NODE_ENV === 'development') {
 	app.use(morgan('dev'));
 }
 
-app.get('/', (req, res, next) => {
-	res.send('API is running...');
-});
-
 app.use('/api/products', productRoutes);
 app.use('/api/users', authRoutes);
 app.use('/api/admin', adminRoutes);
@@ -41,6 +37,18 @@ app.get('/api/config/paypal', (req, res) =>
 
 const __dirname = path.resolve();
 app.use('/images', express.static(path.join(__dirname, '/images')));
+
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static(path.join(__dirname, '/client/build')));
+
+	app.get('*', (req, res, next) =>
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+	);
+} else {
+	app.get('/', (req, res, next) => {
+		res.send('API is running...');
+	});
+}
 
 app.use(notFound);
 
